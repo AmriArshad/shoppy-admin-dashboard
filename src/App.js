a0bc523dea7fcb250d5a7f8d2beaf1d8194f141e
Amri <amri.arshad16@yahoo.com>
2022-10-18 23:59:32 +1300

    Apps

--- src/App.js
+++ src/App.js
@@ -52,9 +52,9 @@ const App = () => {
                 <Route path="/customers" element={<Customers />} />
 
                 {/* Apps */}
+                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/colour-picker" element={<ColourPicker />} />
 
                 {/* Charts */}
