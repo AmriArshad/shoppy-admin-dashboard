7f219d6295e40e3d90cf8d75d62e2c9201eb4eea
Amri <amri.arshad16@yahoo.com>
2022-10-19 21:48:22 +1300

    fixed window reisize bug

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = ({ id, type, data, color, currentColor, height, width }) => {
-  return (
-    <SparklineComponent
-      id={id}
-      type={type}
-      dataSource={data}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      xName="x"
-      yName="yval"
-      tooltipSettings={{
-        visible: true,
-        format: '${x}: data ${yval}',
-        trackLineSettings: {
-          visible: true
-        }
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  );
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, type, data, color, currentColor, height, width } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        type={type}
+        dataSource={data}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        xName="x"
+        yName="yval"
+        tooltipSettings={{
+          visible: true,
+          format: '${x}: data ${yval}',
+          trackLineSettings: {
+            visible: true
+          }
+        }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
 };
 
 export default SparkLine;
